3015d7149b519ed54a7322a70e950f2015040b2f
Kamil Makoś <makos.kamil@o2.pl>
2018-07-18 14:48:12 +0200

    send_test_png

--- modules/handlers.js
+++ modules/handlers.js
@@ -1,7 +1,16 @@
+var fs = require('fs');
+var formidable = require('formidable');
+
 exports.upload = function(request, response) {
     console.log("Rozpoczynam obsługę żądania upload.");
-    response.write("Rozpoczynam upload!");
-    response.end();
+    var form = new formidable.IncomingForm();
+    form.parse(request, function(error, fields, files) {
+        fs.renameSync(files.upload.path, "test.png");
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write("received image:<br/>");
+        response.write("<img src='/show' />");
+        response.end();
+    });
 }
 
 exports.welcome = function(request, response) {
