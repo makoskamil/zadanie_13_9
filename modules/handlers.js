f6a8df0d745dc26f89f369fe29609c4014227381
Kamil Makoś <makos.kamil@o2.pl>
2018-07-18 16:46:01 +0200

    add_files_and_fix

--- modules/handlers.js
+++ modules/handlers.js
@@ -1,16 +1,31 @@
 var fs = require('fs');
 var formidable = require('formidable');
+var mv = require('mv');
+var http = require('http');
+var urlFile;
+var readDir;
 
 exports.upload = function(request, response) {
-    console.log("Rozpoczynam obsługę żądania upload.");
-    var form = new formidable.IncomingForm();
-    form.parse(request, function(error, fields, files) {
-        fs.renameSync(files.upload.path, "test.png");
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.write("received image:<br/>");
-        response.write("<img src='/show' />");
-        response.end();
-    });
+	console.log("Rozpoczynam obsługę żądania upload.");
+	var form = new formidable.IncomingForm();
+	form.uploadDir = "./uploaded_files"; 
+	form.keepExtensions = true;
+	form.parse(request, function(error, fields, files){
+		
+		urlFile = "uploaded_files/" + files.upload.name;
+		urlFile = urlFile.toString();
+		
+		mv(files.upload.path, urlFile, function(err)
+		{if(err) throw error;
+		console.log('Files moved succesfully');
+		});
+	fs.readFile('templates/upload.html',function(err, html){
+			response.writeHead('200', {'Content-Type': "text/html; charset=utf-8"});
+			response.write(html);
+			response.write("<img src='/show' />");
+			response.end();
+		});
+	});
 }
 
 exports.welcome = function(request, response) {
@@ -26,4 +41,12 @@ exports.error = function(request, response) {
     console.log("Nie wiem co robić.");
     response.write("404 :(");
     response.end();
+}
+
+exports.show = function(request, response) {
+    fs.readFile("test.png", "binary", function(error, file) {
+        response.writeHead(200, {"Content-Type": "image/png"});
+        response.write(file, "binary");
+        response.end();
+    });
 }
--- modules/server.js
+++ modules/server.js
@@ -1,13 +1,11 @@
 var http = require('http');
 var colors = require('colors');
-
 var handlers = require('./handlers');
 
 function start() {
   function onRequest(request, response) {
     console.log("Odebrano zapytanie.".green);
     console.log("Zapytanie " + request.url + " odebrane.");
-
     response.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
 
     switch (request.url) { // switch rozróżniający zapytania
@@ -24,7 +22,6 @@ function start() {
   }
 
   http.createServer(onRequest).listen(9000);
-
   console.log("Uruchomiono serwer!".green);
 }
 
